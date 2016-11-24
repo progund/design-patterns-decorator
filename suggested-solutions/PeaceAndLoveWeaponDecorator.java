449890db1ad4bdb040f562a2668dba545a2c8467
Rikard Fröberg <rikard@morus.se>
2016-11-24 23:07:26 +0100

    Fixed bug according to Jozsef's suggestion

--- suggested-solutions/PeaceAndLoveWeaponDecorator.java
+++ suggested-solutions/PeaceAndLoveWeaponDecorator.java
@@ -1,20 +1,13 @@
 public class PeaceAndLoveWeaponDecorator extends WeaponDecorator{
-  private boolean isSet;
+
   public PeaceAndLoveWeaponDecorator(WeaponBehavior weapon){
     super(weapon);
   }
   @Override
   public int useWeapon(){
-    // Debug: to see how each wrapped object has its
-    // own isSet, uncomment below:
-    // System.out.println("My isSet variable is: " + isSet);
-    if ( ! isSet ){
-      System.out.println("Peace, Dude! Healing you with love!");
-      isSet = true;
-      return 0 - super.useWeapon();
-    }else{
-      return super.useWeapon();
-    }
+    System.out.println("Peace, Dude! Healing you with love!");
+    // Jozsef's idea (quite smarter than Rikard's stupid idea)
+    return 0 - Math.abs(super.useWeapon());
   }
   @Override
   public String toString(){
